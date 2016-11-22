df79e5230d34f5032f44c2a513ad300c2d9655ae
xiongpanchen <cxp4220378@sina.com>
2016-11-22 14:25:16 +0800

    tomcat安装脚本文件

--- install_all/inst-tomcat.sh
+++ install_all/inst-tomcat.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+﻿#!/bin/bash
 
 
 	mkdir  /tomcat
@@ -11,14 +11,14 @@
 	#mv /home/tomcat.tar.gz /tomcat
 	tar -zxvf /tomcat/tomcat.tar.gz 
 	#mv /tomcat/tomcat /tomcat/tomcat_1
-	cd /tomcat/tomcat_1
+	cd /tomcat/tomcat
     sudo docker load </home/tomcat.tar
     docker run --name tomcat -p 8080:8080 -v $PWD/webapps:/usr/local/tomcat/webapps -v $PWD/conf:/usr/local/tomcat/conf -v $PWD/logs:/usr/local/tomcat/logs  -d tomcat_jdk1.8
 	#修改nginx默认配置的映射地址
-	cd /nginx/nginx_1/conf
-	chmod 777 sever.conf
-	sed -i -e 's|115.28.150.165:8080|totom:8080|' sever.conf
-	chmod 644 sever.conf
-	exit 0
+	#cd /nginx/nginx_1/conf
+	#chmod 777 sever.conf
+	#sed -i -e 's|115.28.150.165:8080|totom:8080|' sever.conf
+	#chmod 644 sever.conf
+	#exit 0
    ;;  
  
