43d855acddfe1c21e9d6c91594430d9d9fa30e39
ZhangGong <sz9214e@qq.com>
2018-06-10 11:17:32 +0800

    增加基于flask的脚本

---
+++ manage/manager.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+
+from flask import Flask
+from flask import render_template
+import os
+
+app = Flask(__name__)
+
+html = """<!DOCTYPE HTML>
+<html>
+<head><title>docker manager</title></head>
+<body>
+welcome use docker manager!</br>
+%s
+</body>
+</html>
+"""
+
+@app.route("/")
+def index():
+	#return render_template('index.html')
+	body = '<a href="/build">git clone source, build war</a>'
+	return html % body
+
+@app.route('/build')
+def build():
+	os.system('~/m a1')
+	body = 'build finish.</br><a href="/">return</a>'
+	return html % body
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0', debug=True)
+
