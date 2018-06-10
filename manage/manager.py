693570473bf7554b42ccdcc6f7ce2d5f6e99ca99
ZhangGong <sz9214e@qq.com>
2018-06-10 11:34:40 +0800

    增加重启docker的菜单

--- manage/manager.py
+++ manage/manager.py
@@ -19,12 +19,19 @@ welcome use docker manager!</br>
 @app.route("/")
 def index():
 	#return render_template('index.html')
-	body = '<a href="/build">git clone source, build war</a>'
+	body = '<a href="/build">git clone source, build war</a></br>'
+	body += '<a href="/restart">restart docker beta && quartz</a></br>'
 	return html % body
 
 @app.route('/build')
 def build():
-	os.system('~/m a1')
+	#os.system('~/m a1')
+	body = 'build finish.</br><a href="/">return</a>'
+	return html % body
+
+@app.route('/restart')
+def restart():
+	os.system('docker restart beta && docker restart quartz')
 	body = 'build finish.</br><a href="/">return</a>'
 	return html % body
 
