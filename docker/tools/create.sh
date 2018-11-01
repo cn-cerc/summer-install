532b90222c112c87057794ed336c72ba1b40a022
黄荣君 <l1091462907@gmail.com>
2018-11-01 17:48:11 +0800

    调整创建容器脚本，随操作系统启动

--- docker/tools/create.sh
+++ docker/tools/create.sh
@@ -14,9 +14,11 @@ cp ~/summer-sample/target/summer-sample-1.3.2.war ~/tomcats/$app/webapps/ROOT.wa
 cp -R ~/summer-install/docker/factory/tomcat-conf/ ~/tomcats/$app/conf/
 
 sudo docker run -d --name $app -p $port:8080 -h $app \
+    --restart=always \
     -v ~/tomcats/$app/webapps:/opt/tomcat/webapps \
     -v ~/tomcats/$app/conf:/opt/tomcat/conf \
     -v ~/tomcats/$app/logs:/opt/tomcat/logs \
+    -v ~/summer-application.properties:/root/summer-application.properties \
     summer/tomcat
 
 #docker exec -it $app /bin/bash
