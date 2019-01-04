4f4df6bea34f58f70bb0f216c9a350aff43c7b81
黄荣君 <l1091462907@gmail.com>
2019-01-04 10:21:09 +0800

    增加常用的 ubuntu 更新脚本

---
+++ update-ubuntu.sh
@@ -0,0 +1,13 @@
+#!bin/bash
+
+# update linux
+apt-get update -y && apt-get upgrade -y && apt install htop -y
+
+# install git
+# apt install git -y && git clone https://github.com/cn-cerc/summer-install.git
+
+# install nginx
+# apt install nginx-full -y
+
+# innstall docker
+# apt install docker.io -y
