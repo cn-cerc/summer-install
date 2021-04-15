df59b233c0dc682b45c4a8da7d3d3e7d8d0a923e
张弓 <sz9214e@qq.com>
2021-04-15 20:28:24 +0800

    Update install-mvn.sh

--- maven/install-mvn.sh
+++ maven/install-mvn.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
 cd ~
-wget http://oss.mimrc.com/tools/linux/apache-maven-3.8.1-bin.tar.gz
-
-tar -xzvf apache-maven-3.8.1-bin.tar.gz
-
-rm -f apache-maven-3.8.1-bin.tar.gz
-
-mv apache-maven-3.8.1 maven
+apt-get install maven
