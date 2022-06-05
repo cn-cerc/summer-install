9ec989136bd81c7d79bc415e5d72d4e4afb3fee8
黄荣君 <l1091462907@gmail.com>
2022-06-05 08:52:25 +0000

    !1 创建webapp镜像构建和运行脚本

---
+++ docker/webapp-docker-image/create-webapp-image.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 原始镜像名称
+SOURCE_IMAGE=diteng-webapp-release
+# 镜像私服地址
+REGISTRY_HOST=registry-vpc.cn-hangzhou.aliyuncs.com
+# 镜像命名空间
+NAMESPACE=diteng-release
+# 接收镜像打包版本，默认 latest
+TAG=$1
+
+# 删除历史镜像
+docker images | grep $SOURCE_IMAGE  | awk '{print $3}' | xargs docker rmi -f
+
+# 登录私服仓库
+# docker login -p password --username=user@aliyun.com $REGISTRY_HOST
+
+# 获取应用程序
+rm -rf ROOT.war
+wget http://172.16.2.42/job/diteng-beta/ws/diteng-app/target/diteng-app-1.0.0-SNAPSHOT.war -O ROOT.war
+
+# 构建应用镜像
+docker build -t $SOURCE_IMAGE:$TAG .
+
+# 标记镜像版本
+docker tag $SOURCE_IMAGE:$TAG $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+
+# 显示镜像列表
+docker images
+
+# 发布镜像版本
+docker push  $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
---
+++ docker/webapp-docker-image/update-webapp-image.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# 原始镜像名称
+SOURCE_IMAGE=diteng-webapp-release
+# 镜像私服地址
+REGISTRY_HOST=registry-vpc.cn-hangzhou.aliyuncs.com
+# 镜像命名空间
+NAMESPACE=diteng-release
+# 接收镜像运行版本，默认 latest
+TAG=$1
+
+# 删除当前容器
+docker ps | grep $SOURCE_IMAGE  | awk '{print $1}' | xargs docker rm -f
+
+# 删除历史镜像
+docker images | grep $SOURCE_IMAGE  | awk '{print $3}' | xargs docker rmi -f
+
+# 显示镜像列表
+docker images
+
+# 登录私服仓库
+docker login -p password --username=user@aliyun.com $REGISTRY_HOST
+
+# 拉取镜像版本
+docker pull  $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+
+# 创建容器版本
+docker run --name app-8101 -d -p 8101:8080 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+sleep 10
+docker run --name app-8102 -d -p 8102:8080 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+
+# docker logs -n512 -f app-8101
