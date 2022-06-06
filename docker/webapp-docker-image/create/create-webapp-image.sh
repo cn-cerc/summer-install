6dd4e37b7ae3ea187f1987e424f04a748673f9b3
HUANG Rongjun <l1091462907@gmail.com>
2022-06-06 15:02:24 +0800

    Update create-webapp-image.sh

--- docker/webapp-docker-image/create/create-webapp-image.sh
+++ docker/webapp-docker-image/create/create-webapp-image.sh
@@ -6,8 +6,10 @@ SOURCE_IMAGE=diteng-webapp-release
 REGISTRY_HOST=registry-vpc.cn-hangzhou.aliyuncs.com
 # 镜像命名空间
 NAMESPACE=diteng-release
-# 接收镜像打包版本，默认 latest
+# 接收镜像打包版本，默认取jenkins打包成功的编号
 TAG=$1
+# WAR包地址
+WAR_HOST=http://172.16.2.42/job/diteng-beta/ws/diteng-app/target/diteng-app-1.0.0-SNAPSHOT.war
 
 # 删除历史镜像
 docker images | grep $SOURCE_IMAGE  | awk '{print $3}' | xargs docker rmi -f
@@ -17,13 +19,10 @@ docker images | grep $SOURCE_IMAGE  | awk '{print $3}' | xargs docker rmi -f
 
 # 获取应用程序
 rm -rf ROOT.war
-wget http://172.16.2.42/job/diteng-beta/ws/diteng-app/target/diteng-app-1.0.0-SNAPSHOT.war -O ROOT.war
+wget $WAR_HOST -O ROOT.war
 
 # 构建应用镜像
-docker build -t $SOURCE_IMAGE:$TAG .
-
-# 标记镜像版本
-docker tag $SOURCE_IMAGE:$TAG $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+docker build -t $SOURCE_IMAGE:$TAG $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
 
 # 显示镜像列表
 docker images
