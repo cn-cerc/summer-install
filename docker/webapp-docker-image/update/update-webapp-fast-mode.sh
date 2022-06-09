021402bc3743abf867d1ebbc799fa290f0b09d4b
HUANG Rongjun <l1091462907@gmail.com>
2022-06-09 09:21:27 +0800

    update

---
+++ docker/webapp-docker-image/update/update-webapp-fast-mode.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# 快速更新模式，适用于有多台机群的情况
+
+# 原始镜像名称
+SOURCE_IMAGE=diteng-webapp-release
+# 镜像私服地址
+REGISTRY_HOST=registry-vpc.cn-hangzhou.aliyuncs.com
+# 镜像命名空间
+NAMESPACE=diteng-release
+# 接收镜像运行版本，默认 latest
+TAG=$1
+
+# 删除当前容器
+docker ps | grep $SOURCE_IMAGE  | awk '{print $1}' | xargs docker rm -f
+
+# 显示镜像列表
+docker images
+
+# 删除历史镜像
+docker images | grep $SOURCE_IMAGE  | awk '{print $3}' | xargs docker rmi -f
+
+# 登录私服仓库
+docker login -p password --username=user@aliyun.com $REGISTRY_HOST
+
+# 拉取镜像版本
+docker pull  $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+
+# 显示镜像列表
+docker images
+
+# 创建容器版本-每个容器CPU权重是 512/1024
+docker run --name app8101 -d -p 8101:8080 --restart always --cpu-shares 512 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+sleep 10
+docker run --name app8102 -d -p 8102:8080 --restart always --cpu-shares 512 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+
+# docker logs -n512 -f app-8101
--- docker/webapp-docker-image/update/update-webapp-local-mode.sh
+++ docker/webapp-docker-image/update/update-webapp-local-mode.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# 本地更新模式，适用于只有1台机子的情况
+
 # 原始镜像名称
 SOURCE_IMAGE=diteng-webapp-release
 # 镜像私服地址
@@ -8,15 +10,6 @@ REGISTRY_HOST=registry-vpc.cn-hangzhou.aliyuncs.com
 NAMESPACE=diteng-release
 # 接收镜像运行版本，默认 latest
 TAG=$1
-
-# 删除当前容器
-docker ps | grep $SOURCE_IMAGE  | awk '{print $1}' | xargs docker rm -f
-
-# 显示镜像列表
-docker images
-
-# 删除历史镜像
-docker images | grep $SOURCE_IMAGE  | awk '{print $3}' | xargs docker rmi -f
 
 # 登录私服仓库
 docker login -p password --username=user@aliyun.com $REGISTRY_HOST
@@ -27,9 +20,19 @@ docker pull  $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
 # 显示镜像列表
 docker images
 
-# 创建容器版本-每个容器CPU权重是 512/1024
-docker run --name app8101 -d -p 8101:8080 --restart always --cpu-shares 512 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
-sleep 10
-docker run --name app8102 -d -p 8102:8080 --restart always --cpu-shares 512 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+# 显示容器列表
+docker ps
 
-# docker logs -n512 -f app-8101
+# 停止1号容器
+docker stop app8101
+docker rm app8101
+# 更新1号容器
+docker run --name app8101 -d -p 8101:8080 --restart always --cpu-shares 512 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
+
+sleep 30
+
+# 停止2号容器
+docker stop app8102
+docker rm app8102
+# 更新2号容器
+docker run --name app8102 -d -p 8102:8080 --restart always --cpu-shares 512 $REGISTRY_HOST/$NAMESPACE/$SOURCE_IMAGE:$TAG
